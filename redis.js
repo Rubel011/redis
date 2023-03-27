72e9e1dfc01cccc10f8598cd22e94ffb62ea0920
Rubel Foridi <rubelforidi@gmail.com>
2023-03-27 17:01:45 +0530

    second

--- redis.js
+++ redis.js
@@ -1,6 +1,6 @@
 const { createClient } = require("redis")
 const client = createClient({
-    url:"redis-cli -u redis://default:L3Sj9NbEpYnIGrnknMcdOoHoD0EVNXLl@redis-14976.c301.ap-south-1-1.ec2.cloud.redislabs.com:14976"
+    url:"redis://default:L3Sj9NbEpYnIGrnknMcdOoHoD0EVNXLl@redis-14976.c301.ap-south-1-1.ec2.cloud.redislabs.com:14976"
 });
 client.connect();
 
